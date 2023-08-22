11dc1b9e9e2c4bd9a7c9acda488bb0846bbf0022
JSH5000 <39937127+JSH5000@users.noreply.github.com>
2023-08-21 19:40:35 -0500

    Initial commit of code, includes Ratatui and sysinfo for base setup

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+use ratatui::{
+    backend::CrosstermBackend,
+    Terminal,
+    widgets::{Block, Borders},
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
+}
